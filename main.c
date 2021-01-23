a02af18e065d000d885569ecb51339e23977eb8f
gabirat <gabirat@onet.pl>
2021-01-23 20:45:45 +0100

    added print err msg

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/mman.h>
+#include "job.h"
+
+#include "master.h"
+#include "worker.h"
 
 void usage() {
     puts("usage: primes number_of_workers primes_file");
 }
 
-int main(int argc, char const *argv[]) {
+void print_err_msg() {
+    perror("Error");
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         usage();
         return EXIT_FAILURE;
@@ -17,6 +26,12 @@ int main(int argc, char const *argv[]) {
         return EXIT_FAILURE;
     }
     char* primes_file_path = argv[2];
+    job* shared_jobs_space = mmap(NULL, sizeof(*shared_jobs_space) * num_workers,
+                                  PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
+    if (shared_jobs_space == MAP_FAILED) {
+        print_err_msg();
+        return EXIT_FAILURE;
+    }
     
 
     return 0;
